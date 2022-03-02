e33a62ee1007f0ed9e7d70ced6c79bcc7ebaaac9
Shalaka Patil <shalaka@shalaka.local>
2022-03-02 13:47:30 +0530

    Implement edit article route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
       @article = Article.new(params.require(:article).permit(:title, :description))
       if @article.save
@@ -21,5 +25,15 @@ class ArticlesController < ApplicationController
       end
     end
 
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article edited successfully!"
+            redirect_to article_path(@article)
+          else
+            render 'edit'
+          end 
 
+
+    end
 end
