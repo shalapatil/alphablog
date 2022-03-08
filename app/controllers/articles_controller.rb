9792cfb55d7ae471ed87eba21caf4b698b9005ad
Shalaka Patil <shalaka@shalaka.local>
2022-03-08 11:17:53 +0530

    Create user table and associate it with articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
 
     def create
       @article = Article.new(params.require(:article).permit(:title, :description))
+      @article.user = User.first
       if @article.save
         flash[:notice] = "Article created successfully!"
         redirect_to article_path(@article)
@@ -27,6 +28,7 @@ class ArticlesController < ApplicationController
 
     def update
         @article = Article.find(params[:id])
+        @article.user = User.first  
         if @article.update(params.require(:article).permit(:title, :description))
             flash[:notice] = "Article edited successfully!"
             redirect_to article_path(@article)
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    has_many :articles
+    validates :name, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    validates :email, presence: true, 
+                        uniqueness: {case_sensitive: false}, 
+                        length: {maximum: 125},
+                        format: {with: VALID_EMAIL_REGEX}
+end
