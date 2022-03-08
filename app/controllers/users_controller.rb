b22ded20b6b8b6d27efd149d065bd078643b4815
Shalaka Patil <shalaka@shalaka.local>
2022-03-08 16:05:47 +0530

    Add edit user flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,25 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
     def create
         @user = User.new(params.require(:user).permit(:name, :email, :password))
         if @user.save 
             redirect_to articles_path
         else
             render 'new'
-        end
-        
+        end        
     end
 
+    def update
+        @user = User.find(params[:id])
+        if @user.update(params.require(:user).permit(:name, :email, :password))
+            redirect_to articles_path
+        else
+            render 'edit'
+        end       
+    end
 end
