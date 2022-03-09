41fab0f518e3b132a271653def175bf36d7065e1
Shalaka Patil <shalaka@shalaka.local>
2022-03-09 19:59:40 +0530

    While create/edit article, consider current user as author

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(params.require(:user).permit(:name, :email, :password))
         if @user.save 
+            session[:user_id] = @user.id
             redirect_to articles_path
         else
             render 'new'
